84a4576fa1d7a6a9909752122cb872ad4b09d69e
HolaMundo <andresgarsan2003@gmail.com>
2022-04-27 10:12:34 +0200

    Formulamos una pregunta

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,6 +2,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Andrés");
         System.out.println("Hola José David");
+        System.out.println("Que tal compañero");
         System.out.println("Yo bien. ¿Tú?");
     }
 }
